cbfce9db39b3564f57fbfc81166920b287244826
ashvalev78 <ashvalev78@gmail.com>
2019-04-19 16:32:00 +0300

    some axios troubles

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,7 +6,8 @@ import { triggerPopup } from './modules/popup';
 
 import { validateInput } from './modules/validate';
 
-// import Inputmask from 'inputmask';
+import Inputmask from 'inputmask';
+import axios from 'axios';
 
 initMenu('nav-bar');
 initToTop('to-top');
@@ -16,25 +17,45 @@ initMoreBtn('more');
 triggerPopup('popup-form', 'contact-popup');
 initBurgerMenu('burger');
 
-// new Inputmask('+9 (999) 999-99-99').mask(document.getElementById('phone'));
+new Inputmask('+9 (999) 999-99-99').mask(document.getElementById('phone'));
 
-// document.forms[0].querySelectorAll<HTMLInputElement>('input').forEach(el => {
-//   el.addEventListener('change', e => {
-//     validateInput(el);
-//   });
-// });
+document.forms[0].querySelectorAll<HTMLInputElement>('input').forEach(el => {
+  el.addEventListener('change', e => {
+    validateInput(el);
+  });
+});
 
-// document.forms[0].addEventListener('submit', e => {
-//   e.preventDefault();
-//   let validate = true;
-//   document.forms[0].querySelectorAll<HTMLInputElement>('input').forEach(el => {
-//     if (!validateInput(el)) {
-//       validate = false;
-//     }
-//   });
+document.forms[0].addEventListener('submit', e => {
+  e.preventDefault();
+  let validate = true;
+  document.forms[0].querySelectorAll<HTMLInputElement>('input').forEach(el => {
+    if (!validateInput(el)) {
+      validate = false;
+    }
+  });
 
-//   if (validate) console.log('EYE');
-// });
+  if (validate) {
+    axios({
+      method: 'post',
+      url: 'http://webiraysport.com/api/mail_notify.php',
+      data: {
+        name: document.querySelector('#fio').innerHTML,
+        company: document.querySelector('#company').innerHTML,
+        phone: document.querySelector('#phone').innerHTML,
+        mail: document.querySelector('#mail').innerHTML,
+        message: document.querySelector('#message').innerHTML
+      }
+    }).then((response) => {
+      if (response.data === 'ok') {
+        triggerPopup('successPopup', '');
+      } else {
+        triggerPopup('failPopup', '');
+      }
+    }).catch(() => {
+      triggerPopup('failPopup', '');
+    });
+  };
+});
 
 let popupBg = document.querySelector('.popup-bg');
 let politics = document.querySelector('#politics');
